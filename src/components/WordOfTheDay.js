73aff2eef17d12bc4487de9d9f464b0e0e835158
KevinJayke <kevin.jayat@gmail.com>
2023-03-07 17:05:13 +0100

    feat: add definition to word

--- src/components/WordOfTheDay.js
+++ src/components/WordOfTheDay.js
@@ -7,8 +7,19 @@ const WordOfTheDay = () => {
   if (error) return <div>Une erreur est survenue</div>;
   if (isLoading) return <div>Chargement...</div>;
 
-  console.log(data);
-  return <div>{data?.mot}</div>;
+  return (
+    <div>
+      <h2>{data?.mot}</h2>
+      <p>
+        {data?.definitions.map((mot) => (
+          <>
+            <p>{mot.nature}</p>
+            <p>{mot.definition}</p>
+          </>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 export default WordOfTheDay;
