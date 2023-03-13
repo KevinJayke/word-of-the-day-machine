f981297d930000a8081389b13e06732b7d863576
KevinJayke <kevin.jayat@gmail.com>
2023-03-13 14:58:08 +0100

    fix: wording on title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import WordOfTheDay from "./components/WordOfTheDay";
 const App = () => {
   return (
     <div className="App">
-      <div>Le mot du jour est</div>
+      <div>Le mot du jour</div>
       <WordOfTheDay />
     </div>
   );
