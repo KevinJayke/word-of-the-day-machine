002f7d27f09163d3276dc121e9f7784b740db1f1
KevinJayke <kevin.jayat@gmail.com>
2023-03-30 14:27:14 +0200

    fix: wording

--- src/components/WordOfTheDay.js
+++ src/components/WordOfTheDay.js
@@ -4,7 +4,7 @@ import fetchWord from "../api/fetchWord";
 const WordOfTheDay = () => {
   const { data, error, isLoading } = useQuery("mot", fetchWord);
 
-  if (error) return <div>Nous sommes désolé, une erreur est survenue</div>;
+  if (error) return <div>Nous sommes désolé, une erreur est survenue !</div>;
   if (isLoading) return <div>Chargement...</div>;
 
   return (
