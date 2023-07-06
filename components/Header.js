625ef8ab9b0a09d64923ca8ea291d443cccb29cf
KevinJayke <kevin.jayat@gmail.com>
2023-07-06 17:10:25 +0200

    fix: title responsive

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   const today = format(new Date(), "dd MMM yyyy", { locale: frCA });
   return (
     <header className="flex justify-between items-end pb-10">
-      <h1 className="text-6xl font-title">Mot du jour</h1>
+      <h1 className="sm:text-6xl text-5xl font-title">Mot du jour</h1>
       <div className="text-right  text-lg leading-4">
         <h2 className="font-sans pt-2 uppercase">Date</h2>
         <h2 className="font-sans pt-2">{today}</h2>
