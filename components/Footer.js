0b1b19a9de09ed478ac064727552ee6c05c06b5b
KevinJayke <kevin.jayat@gmail.com>
2023-07-08 11:19:48 +0200

    fix: typo

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <footer className="text-center sm:text-xl text-lg font-sans">
-      <h3 className="pt-4">******** Développé par ********</h3>
+      <h3 className="pt-4">******** Visitez ********</h3>
       <h3>
         <a
           target="_blank"
           href="https://hello-kevin.netlify.app/"
           className="py-1 px-2 bg-black text-white">
-          Kévin Jayat
+          mon site
         </a>
       </h3>
     </footer>
